4240c760e77b67a0e3b8fa0a39e467f5aefc4f62
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2021-12-05 00:20:46 +0300

    feat:add response

---
+++ pages/detail.js
@@ -0,0 +1,13 @@
+import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
+import Card from "../components/Card";
+const detail = () => {
+  return (
+    <>
+      <Header />
+      <div></div>
+      <Footer />
+    </>
+  );
+};
+export default detail;
