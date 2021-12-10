a451118d048833765ab5a7bc575d639ff6385c09
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2021-12-10 20:13:33 +0300

    fix

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,39 +1,8 @@
-import Header from "../components/shared/Header";
-import Head from "next/dist/shared/lib/head";
-import Footer from "../components/shared/Footer";
-import Card from "../components/Card";
-
-const portfolio = () => {
+const Portfolio = () => {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <div className=" w-9/12 m-auto flex-1">
-          <div className="grid sm:grid-cols-3  mt-10">
-            <Card
-              image={"/img/tv.png"}
-              link={"https://televizyononarim.com"}
-              paragraph={
-                "Seo compatible React project we made using Nextjs and Tailwind library"
-              }
-            />
-            <Card
-              image={"/img/devnoty.png"}
-              link={"https://devnoty.com"}
-              paragraph={
-                "The web and mobile software company we are affiliated with"
-              }
-            />
-          </div>
-        </div>
-
-        <Footer />
-      </div>
+      <p>Yakında..</p>
     </>
   );
 };
-export default portfolio;
+export default Portfolio;
