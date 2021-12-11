eb6360ebc6fd4a06a63b9b19f67008b62fbf24e6
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2021-12-11 18:33:28 +0300

    logo fix

--- components/ClientProjectCard.js
+++ components/ClientProjectCard.js
@@ -22,7 +22,7 @@ const ClientProjectCard = ({ projects }) => {
             ></Image>
           </div>
         </div>
-        <div className="flex justify-center items-center py-1">
+        <div className="flex justify-center items-center py-2">
           <img
             className=" w-5 h-6"
             src="https://d33wubrfki0l68.cloudfront.net/06745f2f70277023b0910a311bd9735572094fc5/49259/assets/images/logo.svg"
